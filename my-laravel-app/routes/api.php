9fc6e09534cd1136cb32737ae4cabbf6db5f001a
kobakakzu0429 <kobakazu10@gmail.com>
2019-07-08 09:53:45 +0900

    add Articles

--- my-laravel-app/routes/api.php
+++ my-laravel-app/routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['api']], function () {
+    Route::resource('articles', 'Api\ArticlesController');
+});
+
+Route::group(['middleware' => ['api']], function () {
+    Route::resource('articles', 'Api\ArticlesController', ['only' => ['index', 'store', 'destroy']]);
+});
